e9a8c16429812048f36b7ae66b304ae252b94740
democodingal <mayurirastogi0@gmail.com>
2024-10-22 22:28:35 +0530

    done

---
+++ main.py
@@ -0,0 +1,12 @@
+#creating different variable
+x=5
+y="mayuri"
+print(x)
+print(y)
+
+codingal= "codingal"
+print(codingal)
+
+#user input
+name=input("enter your name")
+print("\n Hello ", name, "\n Welcome to codingal")
